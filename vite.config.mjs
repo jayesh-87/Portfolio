75068b70d2e79d755645e20387232f14b9ba96a4
jayesh-87 <125264567+jayesh-87@users.noreply.github.com>
2025-11-28 13:45:07 +0530

    Updated portfolio with new project

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./",
+  base: '/Portfolio/',
 });
+
+
+// export default defineConfig({
+//   base: '/Portfolio/',  // <-- your repository name EXACTLY
+// });
